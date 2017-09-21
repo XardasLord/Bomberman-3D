3f3365c44ed03f3efb9a2ea8d521ba916cfc1089
Paweł Kowalewicz <kowalewicz.pawel@gmail.com>
2017-09-21 20:40:01 +0200

    Generate walls
    Script to generate walls automatically according to the ground size.

---
+++ Assets/Scripts/StaticWallGenerator.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class StaticWallGenerator : MonoBehaviour {
+
+    public GameObject staticWall;
+    public GameObject ground;
+
+    int possitionSeparator = 2;
+
+	void Start ()
+    {
+        GenerateWalls();
+    }
+
+    void GenerateWalls()
+    {
+        Renderer groundRendrerer = ground.GetComponent<Renderer>();
+        Vector3 leftBottomCorner = groundRendrerer.bounds.min + new Vector3(1.5f, .5f, 1.5f);
+        Vector3 rightUpperCorner = groundRendrerer.bounds.max + new Vector3(-1.5f, .5f, -1.5f);
+        Vector3 currentWallPossition = leftBottomCorner;
+
+        for (int x = 0; x <= Mathf.Abs(rightUpperCorner.x * 2); x += 2)
+        {
+            for (int z = 0; z <= Mathf.Abs(leftBottomCorner.z * 2); z += 2)
+            {
+                currentWallPossition = leftBottomCorner + new Vector3(x, 0, z);
+
+                Instantiate(staticWall, currentWallPossition, Quaternion.identity);
+            }
+        }
+    }
+}
