022a28161a33b048eb01dcd5361ff7897e3a2aaa
Paweł Kowalewicz <kowalewicz.pawel@gmail.com>
2017-09-25 20:15:09 +0200

    Planting bomb and explosion effect.
    Added planting bomb action and very simple bomb explosion effect.

---
+++ Assets/Scripts/BombExplosion.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class BombExplosion : MonoBehaviour {
+
+    public GameObject explosionEffect;
+
+    void Start ()
+    {
+        Invoke("Explode", 3f);
+    }
+
+    void Explode()
+    {
+        Instantiate(explosionEffect, transform.position, Quaternion.identity);
+
+        GetComponent<MeshRenderer>().enabled = false;
+        //transform.FindChild("Collider").gameObject.SetActive(false);
+        Destroy(gameObject, .3f);
+    }
+}
---
+++ Assets/Scripts/PlayerAction.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class PlayerAction : MonoBehaviour
+{
+    public float speed = 100f;
+    public GameObject bomb;
+
+    void FixedUpdate()
+    {
+        Move();
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            PlantBomb();
+        }
+    }
+
+    void Move()
+    {
+        var move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        var rigidbody = GetComponent<Rigidbody>();
+
+        rigidbody.velocity = move * speed * Time.deltaTime;
+    }
+
+    void PlantBomb()
+    {
+        var position = new Vector3(Mathf.RoundToInt(transform.position.x), transform.transform.position.y, Mathf.RoundToInt(transform.position.z));
+        Instantiate(bomb, position, Quaternion.identity);
+    }
+}
